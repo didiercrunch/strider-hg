cfbf2aa42fe7c907e946143907f528af1b479077
didier.amyot <didier.amyot@gmail.com>
2014-01-20 10:02:22 -0500

    started to changed the code.  Added tests.

--- get_branches.py
+++
@@ -1,6 +0,0 @@
-from mercurial import ui, hg, node
-from sys import argv
-
-peer = hg.peer(ui.ui(), {}, argv[1])
-for name, rev in peer.branchmap().items():
-    print(name)
---
+++ test/test_lib.js
@@ -0,0 +1,27 @@
+var expect = require('expect.js')
+  , utils = require('../lib/index.js')
+
+describe('The lib util interface', function () {
+  describe('the Hg object', function () {
+    it('should always pass', function(){
+        expect(1).to.be(1);
+    });
+    var hg = new utils.Hg();
+    it('should get the branched name from the script stdout', function () {
+      var stdout = "a\nb\nc\n";
+      expect(hg.getNameOfMercurialBranchesOutOfScriptStdout(stdout)).to.eql(["a", "b", "c"]);
+    })
+  })
+  
+  describe('the config object', function(){
+    it('should be able to initiate properly', function () {
+      var config = new utils.Config();
+      expect(config.url).to.eql("");
+      expect(config.auth.username).to.eql("");
+      config = new utils.Config({url: "1", auth: {username: "2"}});
+      expect(config.url).to.eql("1");
+      expect(config.auth.username).to.eql("2");
+    })
+  
+  })
+})
